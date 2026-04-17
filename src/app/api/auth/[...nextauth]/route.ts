0ea7dcd57108ce9ae448ec3776a36077ecc39e16
tt <xiaotian3303@outlook.com>
2026-04-17 16:46:44 +0800

    fix: nextauth() 放回 auth 解决类型错误

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import NextAuth from "next-auth";
 import { authOptions } from "@/auth"; // 注意：这里 auth.ts 需要只导出 authOptions
 
-const handler = NextAuth(authOptions);
+const handler = authOptions;
 
 export { handler as GET, handler as POST };
